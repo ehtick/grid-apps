bacaa58ceedee4d07d90987537f5c18b250acea7
Stewart Allen <stewart.allen@gmail.com>
2024-06-14 20:43:35 -0400

    attempt to re-create sym links on windows

--- bin/install-pre.js
+++ bin/install-pre.js
@@ -1,3 +1,4 @@
+const os = require('os');
 const fs = require('fs-extra');
 const fetchr = import('node-fetch');
 const path = require('path');
@@ -37,6 +38,29 @@ async function main() {
         "https://static.grid.space/gapp/manifold.wasm",
         path.join("src", "wasm", "manifold.wasm")
     );
+
+    const links = fs.readFileSync("links.csv")
+        .toString()
+        .split('\n')
+        .map(line => line.split(','));
+
+    console.log({ links, platform: os.platform() });
+
+    if (os.platform() === 'win32')
+    for (let [ link, target ] of links) {
+        try {
+            console.log('unlink', link)
+            // Remove existing link if it exists
+            if (fs.existsSync(link)) {
+                fs.unlinkSync(link);
+            }
+            // Create the symlink
+            console.log(`symlink: ${link} -> ${target}`);
+            fs.symlinkSync(target, link, 'junction'); // 'junction' is used for directories on Windows
+        } catch (err) {
+            console.error(`Error creating symlink: ${link} -> ${target}`, err);
+        }
+    }
 }
 
 main().catch(err => console.error('Error', err));
